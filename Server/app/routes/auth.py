b7710ee0df9acbfd9bd750d34f074b06566b7faa
shashank <shashankgundas1@gmail.com>
2025-01-10 23:57:23 +0530

    Implemented signin route

--- Server/app/routes/auth.py
+++ Server/app/routes/auth.py
@@ -1,11 +1,13 @@
 import os
 
+from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException
+from schemas.auth import Signup
 from supabase import Client, create_client
 
-from ..schema.auth import Signup
-
 router = APIRouter(tags=["Authentication"])
+
+load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL") or ""
 key: str = os.environ.get("SUPABASE_ANON_KEY") or ""
@@ -27,10 +29,33 @@ async def signup(data: Signup, provider: str = "Email"):
             {"email": data.email, "password": data.password}
         )
 
-        # Check for errors during sign-up:
-        if not response.get("user"):
+        # response = response.json()
+        # print(type(response))
+        return response
+
+        # # Check for errors during sign-up:
+        if not response["user"]:
             raise HTTPException(status_code=400, detail="Sign-up failed")
 
         return {"user_id": response["user"]["id"]}
+    else:
+        raise HTTPException(status_code=400, detail="Unsupported provider")
+
+
+@router.post("/signin")
+async def signin(data: Signup, provider: str = "Email"):
+    if provider == "Email":
+
+        if not data.email or not data.password:
+            raise HTTPException(
+                status_code=400, detail="Email and password are required"
+            )
+
+        response = supabase.auth.sign_in_with_password(
+            {"email": data.email, "password": data.password}
+        )
+
+        return response
+
     else:
         raise HTTPException(status_code=400, detail="Unsupported provider")
