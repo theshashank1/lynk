5e873965144adecead6f596218751656a8cf4421
shashank <shashankgundas1@gmail.com>
2025-01-12 22:20:10 +0530

    Fixed main.py

--- Server/app/main.py
+++ Server/app/main.py
@@ -1,15 +1,12 @@
+from database import init_db
 from fastapi import FastAPI, Request
+from routes import auth
 
 app = FastAPI()
 
+init_db()
 
-
-
-
-
-
-
-
+app.include_router(auth.router)
 
 
 @app.post("/test")
@@ -17,7 +14,9 @@ async def test(request: Request):
     body = await request.body()
     headers = request.headers
     return {"body": body.decode(), "headers": dict(headers)}
-    
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
