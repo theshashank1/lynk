4770d10d426e1e083ae5224d14761a79a31b4160
shashank <shashankgundas1@gmail.com>
2025-01-29 22:30:51 +0530

    Removed visibility in the database model

--- Server/app/models/media.py
+++ Server/app/models/media.py
@@ -13,7 +13,6 @@ class Video(SQLModel, table=True):  # type: ignore[call-arg]
     thumbnail_url: Optional[str] = None  # URL of the thumbnail
     media_type: str = Field(nullable=False, max_length=20)  # Type (e.g., video, audio)
     status: str = Field(default="public", max_length=20)  # Status of the media
-    visibility: str = Field(default="public", max_length=20)  # Visibility of the media
     tags: Optional[List[str]] = Field(
         default=None, sa_column_kwargs={"type_": "ARRAY(TEXT)"}
     )  # Array of tags
