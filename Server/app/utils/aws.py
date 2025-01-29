e56129dc197b2ea76b78a88f8119663343507776
shashank <shashankgundas1@gmail.com>
2025-01-29 20:59:45 +0530

    Updated env variables

--- Server/app/utils/aws.py
+++ Server/app/utils/aws.py
@@ -14,7 +14,7 @@ def get_aws_session():
     return boto3.Session(
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-        region_name=os.getenv("AWS_REGION_NAME"),
+        region_name=os.getenv("AWS_DEFAULT_REGION"),
     )
 
 
