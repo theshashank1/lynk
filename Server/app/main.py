94bc38c352ffa80dbf72535ad4f49762c09ec7e6
shashank <shashankgundas1@gmail.com>
2025-01-30 13:53:34 +0530

    Created a state variable of supabase client on event startup

--- Server/app/main.py
+++ Server/app/main.py
@@ -1,6 +1,7 @@
 from database import init_db
 from fastapi import FastAPI, Request
 from routes import auth, media
+from utils.supabase import get_supabase_client
 
 app = FastAPI()
 
@@ -8,6 +9,7 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     init_db()
+    app.state.supabase = get_supabase_client()
 
 
 app.include_router(auth.router)
